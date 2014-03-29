97616251b10c835b8d88ca52fccbd7ea36854360
Piotr Pietrzyk <piotrpietrzyk88@gmail.com>
2014-03-29 15:44:18 +0100

    AnkiMessage added.

---
+++ android/Memoling/src/app/memoling/android/anki/entity/AnkiMessage.java
@@ -0,0 +1,32 @@
+package app.memoling.android.anki.entity;
+
+import java.util.List;
+
+import app.memoling.android.entity.Memo;
+
+public class AnkiMessage {
+
+	private final List<Memo> internalMemos;
+	
+	private final List<Memo> externalMemos;
+	
+	private final String destinationMemoBaseId;
+	
+	AnkiMessage(String destinationMemoBaseId, List<Memo> internalMemos, List<Memo> externalMemos){
+		this.internalMemos = internalMemos;
+		this.externalMemos = externalMemos;
+		this.destinationMemoBaseId = destinationMemoBaseId;
+	}
+
+	public List<Memo> getInternalMemos() {
+		return internalMemos;
+	}
+
+	public List<Memo> getExternalMemos() {
+		return externalMemos;
+	}
+
+	public String getDestinationMemoBaseId() {
+		return destinationMemoBaseId;
+	}
+}
